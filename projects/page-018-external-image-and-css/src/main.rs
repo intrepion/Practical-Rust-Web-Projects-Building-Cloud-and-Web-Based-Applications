d0968f0713e082f909bc0f0ac062e0c58383346c
Oliver Forral <intrepion@gmail.com>
2022-01-01 20:41:31 -0700

    copied images and css from github repo

--- projects/page-018-external-image-and-css/src/main.rs
+++ projects/page-018-external-image-and-css/src/main.rs
@@ -1,4 +1,4 @@
-use actix_files::NamedFile;
+use actix_files::{Files, NamedFile};
 use actix_web::{web, App, HttpServer};
 use std::io;
 
@@ -9,8 +9,12 @@ async fn index() -> actix_web::Result<actix_files::NamedFile> {
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     println!("Listing on 127.0.0.1 with port 8080");
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/static", "static").show_files_listing())
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
