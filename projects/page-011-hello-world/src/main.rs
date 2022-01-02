a53cca5aed8996c9a3ac1c1acdb5e471d6be781c
Oliver Forral <intrepion@gmail.com>
2022-01-01 19:58:48 -0700

    refactored hello world

--- projects/page-011-hello-world/src/main.rs
+++ projects/page-011-hello-world/src/main.rs
@@ -1,17 +1,15 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::io;
 
-async fn hello() -> impl Responder {
+async fn hello() -> impl actix_web::Responder {
     HttpResponse::Ok().body("Hello world")
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     println!("Listing on 127.0.0.1 with port 8080");
-    HttpServer::new(|| {
-        App::new().route("/hello", web::get().to(hello))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/hello", web::get().to(hello)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
