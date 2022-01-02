178987c4de951c264440ef07f8621a3b76b11b28
Oliver Forral <intrepion@gmail.com>
2022-01-01 20:47:30 -0700

    copied code from previous project

--- projects/page-022-dynamic-templates/src/main.rs
+++ projects/page-022-dynamic-templates/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::{Files, NamedFile};
+use actix_web::{web, App, HttpServer};
+use std::io;
+
+async fn index() -> actix_web::Result<actix_files::NamedFile> {
+    Ok(NamedFile::open("./static/index.html")?)
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    println!("Listing on 127.0.0.1 with port 8080");
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/static", "static").show_files_listing())
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
