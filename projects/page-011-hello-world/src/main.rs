bbec37d4cb9de8f808fefdf7da5bdadcc45dc851
Oliver Forral <intrepion@gmail.com>
2022-01-01 19:15:59 -0700

    cargo run

--- projects/page-011-hello-world/src/main.rs
+++ projects/page-011-hello-world/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::io;
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    println!("Listing on 127.0.0.1 with port 8080");
+    HttpServer::new(|| {
+        App::new().route("/hello", web::get().to(hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
