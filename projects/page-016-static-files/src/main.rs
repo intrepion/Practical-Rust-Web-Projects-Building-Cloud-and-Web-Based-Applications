6a126fe28c1ea725d98357256e66f164fa990d96
Oliver Forral <intrepion@gmail.com>
2022-01-01 20:04:34 -0700

    initial simple static file

--- projects/page-016-static-files/src/main.rs
+++ projects/page-016-static-files/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::{NamedFile};
+use actix_web::{web, App, HttpServer};
+use std::io;
+
+async fn index() -> actix_web::Result<actix_files::NamedFile> {
+    Ok(NamedFile::open("./static/index.html")?)
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    println!("Listing on 127.0.0.1 with port 8080");
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
