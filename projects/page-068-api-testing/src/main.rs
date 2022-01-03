5ad9d9f62f0c83ccbf0d16574e644110f4817cff
Oliver Forral <intrepion@gmail.com>
2022-01-02 21:52:57 -0700

    Added testing

--- projects/page-068-api-testing/src/main.rs
+++ projects/page-068-api-testing/src/main.rs
@@ -28,13 +28,17 @@ async fn cats_endpoint(
     Ok(HttpResponse::Ok().json(cats_data))
 }
 
-#[actix_web::main]
-async fn main() -> io::Result<()> {
+fn setup_database() -> DbPool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(&database_url);
-    let pool = r2d2::Pool::builder()
+    r2d2::Pool::builder()
         .build(manager)
-        .expect("Failed to create DB connection pool.");
+        .expect("Failed to create DB connection pool.")
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    let pool = setup_database();
 
     println!("Listing on 127.0.0.1 with port 8080");
     HttpServer::new(move || {
@@ -46,4 +50,24 @@ async fn main() -> io::Result<()> {
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, App};
+
+    #[actix_rt::test]
+    async fn test_cats_endpoint_get() {
+        let pool = setup_database();
+        let mut app = test::init_service(
+            App::new()
+                .data(pool.clone())
+                .route("/api/cats", web::get().to(cats_endpoint)),
+        )
+        .await;
+        let req = test::TestRequest::get().uri("/api/cats").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+    }
 }
