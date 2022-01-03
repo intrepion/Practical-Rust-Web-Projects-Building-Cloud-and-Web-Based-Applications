9d0d14a5703c7e33e9b241f861e522b943621e81
Oliver Forral <intrepion@gmail.com>
2022-01-03 14:56:31 -0700

    copied code from previous project

--- projects/page-118-cleaning-up-unresponsive-clients/src/main.rs
+++ projects/page-118-cleaning-up-unresponsive-clients/src/main.rs
@@ -1,3 +1,27 @@
+extern crate ws;
+
+use std::thread;
+use std::time::Duration;
+use ws::WebSocket;
+
+#[allow(dead_code)]
+struct Server {
+    out: ws::Sender,
+}
+
+impl ws::Handler for Server {}
+
 fn main() {
-    println!("Hello, world!");
+    let server = WebSocket::new(|out| Server { out }).unwrap();
+
+    let broadcaster = server.broadcaster();
+
+    let periodic = thread::spawn(move || loop {
+        broadcaster.send("Meow!").unwrap();
+        thread::sleep(Duration::from_secs(1));
+    });
+
+    server.listen("127.0.0.1:8080").unwrap();
+
+    periodic.join().unwrap();
 }
