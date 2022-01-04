f5878464cfa00ff6b7d7ee0933eba5c55c63ec41
Oliver Forral <intrepion@gmail.com>
2022-01-04 05:19:42 -0700

    cargo build --release --target x86_64-unknown-linux-musl && zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap

--- projects/page-147-hello-world-in-lambda/src/main.rs
+++ projects/page-147-hello-world-in-lambda/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use simple_logger::SimpleLogger;
+
+#[derive(serde_derive::Deserialize)]
+struct CustomEvent {
+    #[serde(rename = "firstName")]
+    first_name: String,
+}
+
+#[derive(serde_derive::Serialize)]
+struct CustomOutput {
+    message: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Debug)
+        .with_utc_timestamps()
+        .init()?;
+    lambda_runtime::lambda!(my_handler);
+
+    Ok(())
+}
+
+fn my_handler(
+    e: CustomEvent,
+    c: lambda_runtime::Context,
+) -> Result<CustomOutput, lambda_runtime::error::HandlerError> {
+    if e.first_name == "" {
+        log::error!("Empty first name in request {}", c.aws_request_id);
+        simple_error::bail!("Empty first name");
+    }
+
+    Ok(CustomOutput {
+        message: format!("Hello, {}!", e.first_name),
+    })
 }
