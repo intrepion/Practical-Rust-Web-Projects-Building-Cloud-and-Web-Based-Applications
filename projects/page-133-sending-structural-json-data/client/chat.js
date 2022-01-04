925c417aaf455ee797c30c8541eba979b34d8c0f
Oliver Forral <intrepion@gmail.com>
2022-01-04 04:23:40 -0700

    sending structural JSON data

--- projects/page-133-sending-structural-json-data/client/chat.js
+++ projects/page-133-sending-structural-json-data/client/chat.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const name = prompt("What is your name?");
+    document.getElementById("name").innerText = name;
+
     const socket = new WebSocket("ws://127.0.0.1:8080");
 
     socket.onmessage = function (event) {
         const messages = document.getElementById("messages");
-        messages.value += `${event.data}\n`;
+        const msg = JSON.parse(event.data);
+        const time = (new Date(Number(msg.received_at)))
+            .toLocaleString("en-US");
+        messages.value += `[${time}] ${msg.name}: ${msg.message}\n`;
     };
 
     const sendButton = document.getElementById("send");
     sendButton.addEventListener("click", (event) => {
         const message = document.getElementById("message");
-        socket.send(message.value);
+        socket.send(
+            JSON.stringify({
+                name: name,
+                message: message.value
+            })
+        );
         message.value = "";
     });
 });
--- projects/page-133-sending-structural-json-data/src/main.rs
+++ projects/page-133-sending-structural-json-data/src/main.rs
@@ -1,10 +1,43 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 extern crate ws;
+
+use serde_json::from_str;
+use std::time;
+use std::time::SystemTime;
+use ws::Message;
+
+#[derive(Deserialize, Serialize)]
+struct JSONMessage {
+    name: String,
+    message: String,
+}
 
 fn main() {
     ws::listen("127.0.0.1:8080", |out| {
-        move |msg| {
-            println!("Received message {}", msg);
-            out.broadcast(msg)
+        move |msg: ws::Message| {
+            let msg_text = msg.as_text().unwrap();
+            if let Ok(json_message) = from_str::<JSONMessage>(msg_text) {
+                let now = SystemTime::now()
+                    .duration_since(time::UNIX_EPOCH)
+                    .expect("Time went backwards");
+                let received_at = now.as_millis();
+                println!(
+                    "{} said: {} at {:?}",
+                    json_message.name, json_message.message, received_at
+                );
+                let output_msg = json!({
+                    "name": json_message.name,
+                    "message": json_message.message,
+                    "received_at": received_at.to_string()
+                });
+
+                out.broadcast(Message::Text(output_msg.to_string()))?;
+            }
+            Ok(())
         }
     })
     .unwrap()
